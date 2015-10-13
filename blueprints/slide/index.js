c37eaf4c1379c0b0cdee0542689d6be67a311060
Robdel12 <robertdeluca19@gmail.com>
2015-10-13 16:08:04 -0500

    Fix generate bug where tempalte names were the app name
    This was due to `normalizeEntityName` being in the slides `index.js`

--- blueprints/slide/index.js
+++ blueprints/slide/index.js
@@ -1,8 +1,6 @@
 module.exports = {
   description: 'Generates a new presentation slide',
 
-  normalizeEntityName: function() {},
-
   afterInstall: function(options) {
     // TODO: Generate slide url in `presentations.js`
   }
