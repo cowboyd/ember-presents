eb3497dbee8452192864f59f5eca954dc80de67f
Robdel12 <robertdeluca19@gmail.com>
2015-10-13 15:34:07 -0500

    Add TODO for generating slide url in `presentations.js`

--- blueprints/slide/index.js
+++ blueprints/slide/index.js
@@ -1,19 +1,9 @@
 module.exports = {
   description: 'Generates a new presentation slide',
 
-  // locals: function(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
+  normalizeEntityName: function() {},
 
   afterInstall: function(options) {
-    console.log(options);
-    // var file = 'app/presentation.js';
-    // var text = "";
-    // var afterText = "import resolver from './helpers/resolver';" + EOL;
-
-    // return this.insertIntoFile(file, text, { after: afterText});
+    // TODO: Generate slide url in `presentations.js`
   }
 };
